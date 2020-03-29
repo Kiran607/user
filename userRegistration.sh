27864a1edc7d3084c6ba26d2ba3bf78624c031e6
Kiran607 <ickirankiru@gmail.com>
2020-03-29 19:04:34 +0530

    [Kiran]Refactor adding mobile validation for user

--- userRegistration.sh
+++ userRegistration.sh
@@ -10,6 +10,7 @@ then
 else
 	echo "Invaild"
 fi
+
 echo "Enter user LastName"
 read lname
 lastName=(^[A-Z][a-z]{3,})$
@@ -19,10 +20,21 @@ then
 else
 	echo "Invaild"
 fi
+
 echo "Enter user Email"
 read mail
 pat="^[a-z]{3,}(|[.]?[0-9a-zA-Z]+)([@])([a-z0-9]+)([.|+|_][a-z]{2,4})(|[.][a-zA-Z]{2})$"
 if [[ $mail =~ $pat ]]
+then
+	echo "Vaild"
+else
+	echo "Invaild"
+fi
+
+echo "Enter Mobile Number"
+read number
+pat=(^[0-9]{2}[ ][0-9]{10})$
+if [[ $number =~ $pat ]]
 then
 	echo "Vaild"
 else
